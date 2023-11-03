7dd5d3021f89bcdfc8cc7a1d40f139127685141d
Matt Sweetman <git@webroo.org>
2023-11-03 15:14:09 +0000

    Remove polls list page and adjust not-found page layout

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,8 +3,8 @@ import Link from 'next/link';
 export default function NotFound() {
   return (
     <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
+      <h1 className="mb-4">Not Found</h1>
+      <p className="mb-4">Could not find requested resource</p>
       <Link href="/">Return Home</Link>
     </div>
   );
