13f1c61ec943e1efee26e081f8253ad460b3e2a6
Matt Sweetman <git@webroo.org>
2023-11-13 14:26:09 +0000

    Refactor mongodb connection logic

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,27 +1,39 @@
 import { Db, MongoClient } from 'mongodb';
 
 declare global {
-  var mongoDbPromise: Promise<Db>;
+  var _mongoDbPromise: Promise<Db>;
 }
 
 if (!process.env.MONGODB_URI) {
   throw new Error('Invalid/Missing environment variable: MONGODB_URI');
 }
 
-const mongoUri = process.env.MONGODB_URI;
-const options = {};
-const dbName = 'polly';
+let mongoDbPromise: Promise<Db>;
+
+async function connect() {
+  console.log('mongodb > connect');
+  return new MongoClient(process.env.MONGODB_URI!, {})
+    .connect()
+    .then(client => client.db('polly'))
+    .then(async db => {
+      console.log('mongodb > create index');
+      await db.collection('polls').createIndex({ id: 1 }, { unique: true });
+      return db;
+    });
+}
 
 export async function connectDB(): Promise<Db> {
-  if (!global.mongoDbPromise) {
-    global.mongoDbPromise = new MongoClient(mongoUri, options)
-      .connect()
-      .then(client => client.db(dbName))
-      .then(async db => {
-        await db.collection('polls').createIndex({ id: 1 }, { unique: true });
-        return db;
-      });
+  if (process.env.NODE_ENV === 'development') {
+    if (!global._mongoDbPromise) {
+      console.log('mongodb > create global connection promise');
+      global._mongoDbPromise = connect();
+    }
+    return global._mongoDbPromise;
+  } else {
+    if (!mongoDbPromise) {
+      console.log('mongodb > create local connection promise');
+      mongoDbPromise = connect();
+    }
+    return mongoDbPromise;
   }
-
-  return global.mongoDbPromise;
 }
