747751de7dc126b25cd9c9cb3b9ba9c161d42c8d
Matt Sweetman <git@webroo.org>
2023-11-14 13:00:52 +0000

    Remove debug logs from mongo lib

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -11,12 +11,10 @@ if (!process.env.MONGODB_URI) {
 let mongoDbPromise: Promise<Db>;
 
 async function connect() {
-  console.log('mongodb > connect');
   return new MongoClient(process.env.MONGODB_URI!, {})
     .connect()
     .then(client => client.db('polly'))
     .then(async db => {
-      console.log('mongodb > create index');
       await db.collection('polls').createIndex({ id: 1 }, { unique: true });
       return db;
     });
@@ -25,13 +23,11 @@ async function connect() {
 export async function connectDB(): Promise<Db> {
   if (process.env.NODE_ENV === 'development') {
     if (!global._mongoDbPromise) {
-      console.log('mongodb > create global connection promise');
       global._mongoDbPromise = connect();
     }
     return global._mongoDbPromise;
   } else {
     if (!mongoDbPromise) {
-      console.log('mongodb > create local connection promise');
       mongoDbPromise = connect();
     }
     return mongoDbPromise;
