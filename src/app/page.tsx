cf15099d07b2aa1fe6c638177b547bb33e4dc12f
Matt Sweetman <git@webroo.org>
2023-11-17 15:01:21 +0000

    Add speech bubbles to parrot

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,14 @@ export default async function HomePage() {
         method="GET"
         className="flex flex-col items-center text-center mb-14"
       >
-        <div className="mt-1 mb-7">
+        <div className="relative mt-2 mb-7">
           <ParrotIcon size={128} />
+          <div className="absolute whitespace-nowrap left-32 top-4">
+            <div className="font-serif italic text-md text-gray-700 bg-gray-100 rounded-md px-2 py-1 rounded-bl-none">
+              Hello!
+            </div>
+            <div className="absolute bottom-0 -left-3 w-3 h-3 bg-[radial-gradient(circle_at_top_left,transparent_0.75rem,rgb(243,244,246)_0.8rem)]"></div>
+          </div>
         </div>
         <label htmlFor="title" className="block mb-5 text-4xl">
           What do you want to organise?
--- src/app/polls/layout.tsx
+++ src/app/polls/layout.tsx
@@ -5,7 +5,13 @@ export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div className="flex flex-col h-full">
       <div className="flex-1">{children}</div>
-      <div className="self-center pt-14 pb-2">
+      <div className="relative self-center pt-14 pb-2">
+        <div className="absolute whitespace-nowrap left-16">
+          <div className="text-xs bg-gray-100 rounded-md px-2 py-1 rounded-bl-none">
+            <a href="/">Want to create your own poll?</a>
+          </div>
+          <div className="absolute bottom-0 -left-2 w-2 h-2 bg-[radial-gradient(circle_at_top_left,transparent_0.5rem,rgb(243,244,246)_0.6rem)]"></div>
+        </div>
         <a href="/">
           <ParrotIcon size={64} />
         </a>
