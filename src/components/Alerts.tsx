2dd5a451c74c59c3ea941b8bf87a383da0263368
Matt Sweetman <git@webroo.org>
2023-11-24 16:37:07 +0000

    Add success icon and alert box

--- src/components/Alerts.tsx
+++ src/components/Alerts.tsx
@@ -1,10 +1,39 @@
 import clsx from 'clsx';
 import { HTMLAttributes } from 'react';
-import { ErrorIcon } from '@/components/Icons';
+import { ErrorIcon, SuccessIcon } from '@/components/Icons';
 
 export interface ErrorAlertProps extends HTMLAttributes<HTMLDivElement> {
   title: string;
   messages?: string[];
+}
+
+export function SuccessAlert({
+  title,
+  messages,
+  className,
+  ...props
+}: ErrorAlertProps) {
+  return (
+    <div
+      className={clsx(
+        'flex gap-3 p-3 bg-green-50 rounded-md text-sm',
+        className,
+      )}
+      {...props}
+    >
+      <SuccessIcon />
+      <div>
+        <h3 className="text-green-800 font-semibold">{title}</h3>
+        {messages && (
+          <ul className="text-green-700 space-y-1 pl-2 mt-1">
+            {messages.map(message => (
+              <li key={message}>{message}</li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export function ErrorAlert({
